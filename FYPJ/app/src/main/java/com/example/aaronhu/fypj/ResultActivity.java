ffcd47b59a8615aa0f4957554c55b1c90e230b45
Aaron Hu <thu5@ualberta.com>
2016-10-05 00:08:19 +0800

    Update ResultActivity.java

--- FYPJ/app/src/main/java/com/example/aaronhu/fypj/ResultActivity.java
+++ FYPJ/app/src/main/java/com/example/aaronhu/fypj/ResultActivity.java
@@ -1,10 +1,14 @@
 package com.example.aaronhu.fypj;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -12,67 +16,59 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-/**
- * Created by aaronhu on 9/24/16.
- */
-public class ResultActivity extends Activity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_result);
+public class StudyActivity extends AppCompatActivity {
+    private static String choice = "";
+    private static int choiceId = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_study);
 
-            Intent intent = getIntent();
-            ArrayList<String>answersheet = intent.getStringArrayListExtra("answersheet");
-            int Score  = 0;
+        ListView lv = (ListView) findViewById(R.id.courseList);
+        final TextView tx = (TextView) findViewById(R.id.choice);
+
+        final ArrayList<String> data = new ArrayList<String>();
 
 
-            ListView lv = (ListView) findViewById(R.id.courseList3);
-            TextView tx = (TextView) findViewById(R.id.score);
-            Button finish = (Button)findViewById(R.id.reviewFinish);
-            Button redo = (Button)findViewById(R.id.redo);
+        data.add("1. Transaction analysis");
+        data.add("2. Journal entries");
+        data.add("3. Trail balance");
+        data.add("4. Posting");
+        data.add("5. Adjusting");
+        data.add("6. Worksheet");
+        data.add("7. Financial statement");
+        data.add("8. Closing entries");
 
-            finish.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    finish();
-                }
-            });
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.my_listitem, data);
+        lv.setAdapter(adapter);
 
-            redo.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent i = new Intent(ResultActivity.this,ExamActivity.class);
-                    startActivity(i);
-                    finish();
-                }
-            });
+        int height = 0;
+        for (int ioo = 0; ioo < data.size(); ioo++) {
+            View item = adapter.getView(ioo, null, lv);
+            item.measure(0, 0);
+            height += item.getMeasuredHeight();
+        }
+        ViewGroup.LayoutParams params = lv.getLayoutParams();
+        params.height = height + (lv.getDividerHeight() * (adapter.getCount() - 1));
+        lv.setLayoutParams(params);
 
-            final ArrayList<String> data = new ArrayList<String>();
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                choice = data.get((int)id);
+                choiceId = (int)id;
+                tx.setText(choice);
 
-            //we should know what the right answers are.
-            for(int i=1;i<=answersheet.size();i++){
-                data.add(i + ".       " + answersheet.get(i - 1) + "        B            " + answersheet.get(i - 1).equals("B"));
-                if(answersheet.get(i-1).equals("B")){
-                    Score+=100/answersheet.size();
-                }
             }
+        });
+    }
 
-           tx.setText("Your Score: "+Score);
+    public void studyOnClick(View view){
 
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.my_listitem, data);
-            lv.setAdapter(adapter);
-
-            int height = 0;
-            for (int ioo = 0; ioo < data.size(); ioo++) {
-                View item = adapter.getView(ioo, null, lv);
-                item.measure(0, 0);
-                height += item.getMeasuredHeight();
-            }
-            ViewGroup.LayoutParams params = lv.getLayoutParams();
-            params.height = height + (lv.getDividerHeight() * (adapter.getCount() - 1));
-            lv.setLayoutParams(params);
-
+        Intent intent = new Intent(StudyActivity.this,BookActivity.class);
+        intent.putExtra("title",choice);
+        intent.putExtra("id",choiceId);
+        startActivity(intent);
 
     }
 }
